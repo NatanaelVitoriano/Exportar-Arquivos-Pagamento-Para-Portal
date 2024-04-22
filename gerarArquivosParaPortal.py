7d6a9d478126828dd270e38cb913e870b7dc692c
natanaelvitor88@gmail.com <NatanaelVitoriano>
2024-04-22 14:20:55 -0300

    Alteracoes no projeto.

--- gerarArquivosParaPortal.py
+++ gerarArquivosParaPortal.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from pathlib import Path
 from arquivoAFeAP import dataAF, dataAP
@@ -8,13 +9,23 @@ print("Digite o codigo do municipio: ")
 numMunicipio = input()
 print("Digite a data de referencia: (OBS: com barras'/') ")
 dataReferencia = input()
-
+print("Descricao do cargo. CBO ou Nome: ")
+cargo = input()
 listaAPs = []
 listaAFs = []
 dicioAFs = []
 
 dicioOrgaos = {}
-orgDados = requests.get('https://api-dados-abertos.tce.ce.gov.br/orgaos?codigo_municipio=' + numMunicipio + '&exercicio_orcamento=202300')
+try:
+    orgDados = requests.get('https://api-dados-abertos.tce.ce.gov.br/orgaos?codigo_municipio=' + numMunicipio + '&exercicio_orcamento=202400')
+except:
+    print("Tentando conexao com api TCE...")
+    try:
+        orgDados = requests.get('https://api-dados-abertos.tce.ce.gov.br/orgaos?codigo_municipio=' + numMunicipio + '&exercicio_orcamento=202400')
+    except:
+        print("Não foi possivel acessar api TCE.")
+        os.system("PAUSE")
+        quit()
 json = orgDados.json()
 listaDeOrgaos = json['data']
 
@@ -41,7 +52,7 @@ for dataAFAgrupada in listaDataAFAgrupada:
 
     index_row = [dataAP.index(row) for row in dataAP if dataAFAgrupada[0][8] in row]
     
-    listaAFs.append((dataAP[index_row[0]][16]).replace('"',"").replace("  ","").strip() + ";" + dataReferencia + ";" + str("%.2f" % totalBruto) + ";" + str("%.2f" % totalSubtraido) + ";" + str("%.2f" % totalLiquido) + ";" + dataAFAgrupada[0][8])
+    listaAFs.append((dataAP[index_row[0]][16]).replace('"',"").replace("  ","").strip().lstrip("0") + ";" + dataReferencia + ";" + str("%.2f" % totalBruto) + ";" + str("%.2f" % totalSubtraido) + ";" + str("%.2f" % totalLiquido) + ";" + dataAFAgrupada[0][8])
 
 arquivoRemuneracaoDoAgente = Path(numMunicipio + "_WEBSERFOL2" + dataReferencia.replace("/","") + ".MAX")
 arquivoRemuneracaoDoAgente.write_text("\n".join(listaAFs), encoding="utf8")
@@ -54,7 +65,7 @@ listaDataAPAgrupada = [[item for item in data] for (key, data) in groups]
 for ap in listaDataAPAgrupada:
     for listaAux in listaAFs:
         if ap[0][5] in listaAux:
-            listaAPs.append(str(ap[0][16].replace('"',"").strip() + ";" + ap[0][16].replace('"',"").strip() + ";" + ap[0][31] + ";" + ap[0][40].replace("\n","").upper() + ";" + ap[0][5] + ";"+ dicioOrgaos[ap[0][3].replace('"',"")].upper() + ";" + dataReferencia).replace("  ","").strip())
+            listaAPs.append(str(ap[0][16].replace('"',"").strip().lstrip("0") + ";" + ap[0][16].replace('"',"").strip().lstrip("0") + ";" + ap[0][31] + ";" + (ap[0][40].replace("\n","").upper() if cargo != "cbo" else ap[0][20]) + ";" + ap[0][5] + ";"+ dicioOrgaos[ap[0][3].replace('"',"")].upper() + ";" + dataReferencia).replace("  ","").strip())
 
 arquivoDadosDoAgente = Path(numMunicipio + "_WEBSERFOL1" + dataReferencia.replace("/","") + ".MAX")
 arquivoDadosDoAgente.write_text("\n".join(listaAPs), encoding="utf8")
